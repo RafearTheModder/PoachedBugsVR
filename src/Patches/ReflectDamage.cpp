35a32c3453226ceaad0c9fcf39ddfb373306478a
RafearTheModder <RafearTheModder@gmail.com>
2023-12-01 00:32:20 -0600

    Correct choice of NOP for ReflectDamage patch
    Was mistakenly using NOP8 instead of NOP2, which was causing an extremely high damage reflection even with 0 reflect damage stat

--- src/Patches/ReflectDamage.cpp
+++ src/Patches/ReflectDamage.cpp
@@ -7,9 +7,7 @@ namespace Patch
         {
             REL::Relocation<uintptr_t> patch{ REL::ID(42842) };
             SKSE::AllocTrampoline(1 << 4);
-            // 0x761 offset from REL::ID(42842) marks the beginning of a section of code that caps reflect damage within a hit event to a maximum of 100%.
-            //    Replacing this section with a NOP8 elimates the cap and allows the full raw value to flow through to the return damage calculation afterwards.
-            REL::safe_write(patch.address() + 0x761, REL::NOP8, 8);
+            REL::safe_write(patch.address() + 0x761, REL::NOP2, 2);
             logger::info("\"Reflect damage limit removal\" patch installed!");
         }
         else
