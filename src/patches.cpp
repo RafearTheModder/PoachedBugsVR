94d343416e55d5c666451b5f147d510c0321e50c
RafearTheModder <RafearTheModder@gmail.com>
2023-12-01 00:32:19 -0600

    Refactor patches .h and .cpp

--- src/patches.cpp
+++ src/patches.cpp
@@ -1,2 +1,18 @@
 #include "patches.h"
 #include "dataHandler.h"
+
+void scrollExpPatch::InstallPatch() {
+    // Replace ScrollItem::GetSkillUsageData with SpellItem::GetSkillUsageData
+    REL::Relocation<uintptr_t> scrollPatch{ REL::ID(228867) }; // ScrollItem::VirtualFunctionTable, index 60 contains GetSkillUsageData
+    REL::Relocation<uintptr_t> spellItemPatch{ REL::ID(228888) }; // SpellItem::VirtualFunctionTable, index 60 contains GetSkillUsageData
+    REL::safe_write(reinterpret_cast<std::uintptr_t>(std::addressof(reinterpret_cast<std::uintptr_t*>(scrollPatch.address())[0x60])), reinterpret_cast<std::uintptr_t*>(spellItemPatch.address())[0x60]);
+    logger::info("Scroll Experience patch installed!");
+};
+
+void reflectDamagePatch::InstallPatch() {
+    // TODO: Revisit address and offset. Address maps, but offset seems to be wrong as evidenced by damage reflection still being capped.
+    REL::Relocation<uintptr_t> patch{ REL::ID(42842) };
+    SKSE::AllocTrampoline(1 << 4);
+    REL::safe_write(patch.address() + 0x544, REL::NOP8, 8);
+    logger::info("Reflect Damage patch installed!");
+};
--- src/patches.h
+++ src/patches.h
@@ -5,13 +5,7 @@
 class scrollExpPatch
 {
 public:
-    static void InstallPatch() {
-        // Replace ScrollItem::GetSkillUsageData with SpellItem::GetSkillUsageData
-        REL::Relocation<uintptr_t> scrollPatch{ REL::ID(228867) };
-        REL::Relocation<uintptr_t> spellItemPatch{ REL::ID(228888) };
-        REL::safe_write(reinterpret_cast<std::uintptr_t>(std::addressof(reinterpret_cast<std::uintptr_t*>(scrollPatch.address())[0x60])), reinterpret_cast<std::uintptr_t*>(spellItemPatch.address())[0x60]);
-		logger::info("Scroll Experience patch installed!");
-    };
+    static void InstallPatch();
 private:
 };
 
@@ -19,12 +13,6 @@ private:
 class reflectDamagePatch
 {
 public:
-    static void InstallPatch() {
-        // TODO: Revisit address and offset. Address maps, but offset seems to be wrong as evidenced by damage reflection still being capped.
-        REL::Relocation<uintptr_t> patch{ REL::ID(42842) };
-        SKSE::AllocTrampoline(1 << 4);
-        REL::safe_write(patch.address() + 0x544, REL::NOP8, 8);
-		logger::info("Reflect Damage patch installed!");
-    };
+    static void InstallPatch();
 private:
 };
