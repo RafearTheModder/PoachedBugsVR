abb629334392e667fa0feeeaf33cda8b40339078
RafearTheModder <RafearTheModder@gmail.com>
2023-12-01 00:32:20 -0600

    Implement poison resistance patch

--- src/Patches/PoisonResistance.cpp
+++ src/Patches/PoisonResistance.cpp
@@ -14,7 +14,7 @@ namespace Patch
             REL::Relocation<uintptr_t> playerMagicTargetVtableAddress{ REL::ID(261401) };
             auto playerCheckResistanceFuncPtr = reinterpret_cast<std::uintptr_t>(std::addressof(reinterpret_cast<std::uintptr_t*>(playerMagicTargetVtableAddress.address())[0xA]));
             REL::safe_write(playerCheckResistanceFuncPtr, reinterpret_cast<std::uintptr_t>(std::addressof(PoisonResistance::CheckResistance)));
-            logger::info("Warning: \"Poison resistance\" patch is not implemented fully yet!");
+            logger::info("\"Poison resistance\" patch installed!");
         }
         else
         {
@@ -23,6 +23,53 @@ namespace Patch
     };
     
     float PoisonResistance::CheckResistance(RE::MagicTarget* a_magicTarget, RE::MagicItem* a_magicItem, RE::Effect* a_effect, RE::TESBoundObject* a_object){
-        return 1.0;
+        if (a_magicItem->hostileCount <=0 || (a_object && a_object->formType == RE::FormType::Armor))
+        {
+            return 1.0;
+        }
+
+        auto type = a_magicItem->GetSpellType();
+        RE::AlchemyItem* potion{nullptr};
+        if (type == RE::MagicSystem::SpellType::kPotion)
+        {
+            potion = static_cast<RE::AlchemyItem*>(a_magicItem);
+        }
+        bool isPoison = false;
+        if (potion)
+        {
+            isPoison = potion->IsPoison();
+        }
+        
+        if ((potion && !isPoison && !a_effect->IsHostile())||\
+            (type == RE::MagicSystem::SpellType::kIngredient && a_magicItem->IsFood()))
+        {
+            return 1.0;
+        }
+
+        auto* a_magicTargetAsActor = static_cast<RE::Actor*>(a_magicTarget);
+        float magicResistance = a_magicTargetAsActor->GetActorValue(RE::ActorValue::kResistMagic);;
+        float specificResistance{0.0};
+        auto actorValueForResistance = a_effect->baseEffect->data.resistVariable;
+
+        if (actorValueForResistance != RE::ActorValue::kNone)
+        {
+            specificResistance = a_magicTargetAsActor->GetActorValue(actorValueForResistance);
+        }
+        
+        if (a_magicTargetAsActor == RE::PlayerCharacter::GetSingleton())
+        {
+            float maxResistanceValue = RE::GameSettingCollection::GetSingleton()->GetSetting("fPlayerMaxResistance")->GetFloat();
+            magicResistance = (magicResistance >= maxResistanceValue ? maxResistanceValue : magicResistance);
+            specificResistance = (specificResistance >= maxResistanceValue ? maxResistanceValue : specificResistance);
+        }
+
+        float finalDamageMult = (specificResistance >= 100.0 ? 0.0 : (1.0 - (specificResistance/100.0)));
+
+        if (!(isPoison || type == RE::MagicSystem::SpellType::kPoison))
+        {
+            finalDamageMult *= (magicResistance >= 100.0 ? 0.0 : (1.0 - (magicResistance/100.0)));
+        }
+
+        return finalDamageMult;
     };
 } // namespace Patch
