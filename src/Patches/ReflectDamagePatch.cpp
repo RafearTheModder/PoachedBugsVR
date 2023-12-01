e0ab7e94d171df30ca26030ed41e4a8e6fd826b6
RafearTheModder <RafearTheModder@gmail.com>
2023-12-01 00:32:19 -0600

    Add comment explaining reflect damage limit removal methodology

--- src/Patches/ReflectDamagePatch.cpp
+++ src/Patches/ReflectDamagePatch.cpp
@@ -7,6 +7,8 @@ namespace Patch
         {
             REL::Relocation<uintptr_t> patch{ REL::ID(42842) };
             SKSE::AllocTrampoline(1 << 4);
+            // 0x761 offset from REL::ID(42842) marks the beginning of a section of code that caps reflect damage within a hit event to a maximum of 100%.
+            //    Replacing this section with a NOP8 elimates the cap and allows the full raw value to flow through to the return damage calculation afterwards.
             REL::safe_write(patch.address() + 0x761, REL::NOP8, 8);
             logger::info("\"Reflect damage limit removal\" patch installed!");
         }
