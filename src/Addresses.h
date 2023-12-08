26e719734e0049268c995aecbd277d60f45850f4
RafearTheModder <RafearTheModder@gmail.com>
2023-12-08 17:15:41 -0600

    Add new Addresses.h file to contain addresses I use throughout the project
    Meant to catch addresses that do not have clean readable names in CommonLib includes

---
+++ src/Addresses.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "REL/Relocation.h"
+
+namespace RE
+{
+	namespace Address
+	{
+        namespace ReflectDamage
+        {
+            inline const REL::Relocation<std::uintptr_t> CompareReflectDamageAV{REL::ID(42842).address() + 0x761};
+        }
+
+        namespace PerkEntry
+        {
+            inline const REL::Relocation<REL::ID> SelectSpellFunctionPointer{REL::ID(675819)};
+            inline const REL::Relocation<REL::ID> SelectSpellFunctionResultCount{REL::ID(502190)};
+
+            namespace HandlePerkEntry
+            {
+                inline const REL::Relocation<std::uintptr_t> SelectBashingSpell{REL::ID(37673).address() + 0x40E};
+                inline const REL::Relocation<std::uintptr_t> SelectCombatHitSpell{REL::ID(37799).address() + 0x61};
+                inline const REL::Relocation<std::uintptr_t> SelectCombatHitArrowProjectileSpell{REL::ID(42547).address() + 0x28B};
+                inline const REL::Relocation<std::uintptr_t> SelectReanimateSpell{REL::ID(37865).address() + 0xBA};
+                inline const REL::Relocation<std::uintptr_t> SelectSneakingSpell{REL::ID(36926).address() + 0xB6};
+                inline const REL::Relocation<std::uintptr_t> SelectWeaponSwingSpell{REL::ID(37628).address() + 0xAB};
+            }
+            
+            namespace ApplySpell
+            {
+                inline const REL::Relocation<std::uintptr_t> ApplyBashingSpell{REL::ID(37673).address() + 0x429};
+                inline const REL::Relocation<std::uintptr_t> ApplyCombatHitSpell{REL::ID(37799).address() + 0x79};
+                inline const REL::Relocation<std::uintptr_t> ApplyCombatHitArrowProjectileSpell{REL::ID(42547).address() + 0x2A7};
+                inline const REL::Relocation<std::uintptr_t> ApplyReanimateSpell{REL::ID(37865).address() + 0xD2};
+                inline const REL::Relocation<std::uintptr_t> ApplyWeaponSwingSpell{REL::ID(37628).address() + 0xC3};
+            }
+        }
+	}
+}
