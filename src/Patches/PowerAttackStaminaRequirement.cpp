bb97c636a336805723488fba270e430e3fd7018d
RafearTheModder <RafearTheModder@gmail.com>
2023-12-10 23:10:57 -0600

    Implement PowerAttackStaminaRequirement
    Closes #8
    
    NOTE: The player side of this appears to be broken due to an external issue with power attacks reporting zero stamina required no matter what out of originalPlayerHasStamina_
    Uncertain if there is a solution to this here...

--- src/Patches/PowerAttackStaminaRequirement.cpp
+++ src/Patches/PowerAttackStaminaRequirement.cpp
@@ -1,10 +1,24 @@
 #include "PowerAttackStaminaRequirement.h"
+#include "SKSE/Trampoline.h"
+
+#include "Addresses.h"
 
 namespace Patch
 {
     void PowerAttackStaminaRequirement::InstallPatch() {
         if(settings.powerAttacksStaminaRequirement)
         {
+            REL::safe_write(RE::Address::PowerAttackStaminaChecks::ActorHasStamina.address(), REL::NOP2, 2);
+            REL::safe_write(RE::Address::PowerAttackStaminaChecks::PlayerHasStamina.address(), REL::NOP2, 2);
+
+            SKSE::Trampoline& trampoline = SKSE::GetTrampoline();
+
+            SKSE::AllocTrampoline(1 << 4);
+            originalActorHasStamina_ = trampoline.write_call<5>(RE::Address::PowerAttackStaminaChecks::GetActorAttackStamina.address(), ActorHasStamina);
+            
+            SKSE::AllocTrampoline(1 << 4);
+            originalPlayerHasStamina_ = trampoline.write_call<5>(RE::Address::PowerAttackStaminaChecks::GetPlayerAttackStamina.address(), PlayerHasStamina);
+
             logger::info("\"PowerAttackStaminaRequirement\" patch installed!");
         }
         else
@@ -14,13 +28,41 @@ namespace Patch
     };
     
     float PowerAttackStaminaRequirement::ActorHasStamina(RE::ActorValueOwner* actorValueOwner, RE::BGSAttackData* attackData){
+        float attackStamina = originalActorHasStamina_(actorValueOwner, attackData);
 
-        return 0.0;
+        float currentStamina = actorValueOwner->GetActorValue(RE::ActorValue::kStamina);
+
+        if (attackStamina <= 0.0)
+        {
+            return attackStamina;
+        }
+        else if (currentStamina >= attackStamina)
+        {
+            return 0.0;
+        }
+        else
+        {
+            return attackStamina;
+        }
     };
     
     float PowerAttackStaminaRequirement::PlayerHasStamina(RE::ActorValueOwner* actorValueOwner, RE::BGSAttackData* attackData){
+        float attackStamina = originalPlayerHasStamina_(actorValueOwner, attackData);
 
-        return 0.0;
+        float currentStamina = actorValueOwner->GetActorValue(RE::ActorValue::kStamina);
+
+        if (attackStamina <= 0.0)
+        {
+            return attackStamina;
+        }
+        else if (currentStamina >= attackStamina)
+        {
+            return 0.0;
+        }
+        else
+        {
+            return attackStamina;
+        }
     };
 
     REL::Relocation<decltype(PowerAttackStaminaRequirement::ActorHasStamina)> PowerAttackStaminaRequirement::originalActorHasStamina_{};
