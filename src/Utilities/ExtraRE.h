3bcb181594a1b00503a3c74b4928e5a5cb0c3b10
RafearTheModder <RafearTheModder@gmail.com>
2024-11-09 21:53:27 -0600

    fix(ImproveAllEnchantmentEffects):reimplement patch mirroring Kernal's implementation for SE 1.5.97
    resolves #42

---
+++ src/Utilities/ExtraRE.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+
+namespace REX{
+    /* These set effect duration/magnitude functions would make the most sense in CommonLib, but I do not know the AE/1.6000000000000000000000000 address lib ids...*/
+	bool SetEffectMagnitude(RE::Effect* effect, float magnitude)
+	{
+		using func_t = decltype(&SetEffectMagnitude);
+		static REL::Relocation<func_t> func{ REL::Offset(0x108a40) };
+		return func(effect, magnitude);
+	}
+
+	bool SetEffectDuration(RE::Effect* effect, std::uint32_t duration)
+	{
+		using func_t = decltype(&SetEffectDuration);
+		static REL::Relocation<func_t> func{ REL::Offset(0x108b00) };
+		return func(effect, duration);
+	}
+
+	// Really shaky on this function's details, so not comfortable submitting it to CommonLib yet
+	template <class T>
+	unsigned int BSTArrayAddElement(RE::BSTArray<T>* array, T* effect)
+	{
+		using func_t = decltype(&BSTArrayAddElement<T>);
+		static REL::Relocation<func_t> func{ REL::Offset(0x88ea20) };
+		return func(array, effect);
+	}
+}
