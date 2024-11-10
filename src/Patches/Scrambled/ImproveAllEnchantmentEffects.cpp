6d5e8ba68a99e9b2e8fb35e58766f834f1818ef0
RafearTheModder <RafearTheModder@gmail.com>
2024-11-09 21:53:27 -0600

    fix(ImproveAllEnchantmentEffects):reimplement patch mirroring Kernal's implementation for SE 1.5.97
    resolves #42

--- src/Patches/Scrambled/ImproveAllEnchantmentEffects.cpp
+++ src/Patches/Scrambled/ImproveAllEnchantmentEffects.cpp
@@ -4,6 +4,7 @@
 */
 
 #include "ImproveAllEnchantmentEffects.h"
+#include "Utilities/ExtraRE.h"
 
 namespace Patch
 {
@@ -19,16 +20,106 @@ namespace Patch
             logger::info("\"ImproveAllEnchantmentEffects\" patch is NOT enabled... skipping.");
         }
     };
-    
+
     RE::BSContainer::ForEachResult ImproveAllEnchantmentEffects::AddEffect(RE::CraftingSubMenus::EnchantConstructMenu::CreateEffectFunctor* createEffectFunctor, RE::Effect* effect){
-        RE::Effect* actualCostliestEffect = createEffectFunctor->costliestEffect;
-        createEffectFunctor->costliestEffect = effect;
+		auto createdEffectIndex = REX::BSTArrayAddElement(&createEffectFunctor->createdEffects, effect);
 
-        auto retval = originalAddEffect_(createEffectFunctor, effect);
+		auto magnitude = effect->GetMagnitude();
+		auto duration  = effect->GetDuration();
 
-        createEffectFunctor->costliestEffect = actualCostliestEffect;
+		auto effectSettingFlags    = effect->baseEffect->data.flags;
+		auto powerAffectsMagnitude = effectSettingFlags.all(RE::EffectSetting::EffectSettingData::Flag::kPowerAffectsMagnitude);
+		auto powerAffectsDuration  = effectSettingFlags.all(RE::EffectSetting::EffectSettingData::Flag::kPowerAffectsDuration);
 
-        return retval;
+		float power{ 1.0F };
+
+		auto enchantmentEntry = createEffectFunctor->enchantmentParams;
+
+		if (enchantmentEntry)
+		{
+			float maximumPower{ 1.0F };
+
+			if (powerAffectsMagnitude)
+			{
+				maximumPower = magnitude;
+			}
+			else if (powerAffectsDuration)
+			{
+				maximumPower = static_cast<float>(duration);
+			}
+
+			auto player          = RE::PlayerCharacter::GetSingleton();
+			auto enchantingSkill = player->GetActorValue(RE::ActorValue::kEnchanting);
+
+			maximumPower = RE::EnchantmentItem::ModifyPower(maximumPower, enchantingSkill);
+			RE::BGSEntryPoint::HandleEntryPoint(RE::BGSEntryPoint::ENTRY_POINTS::kModEnchantmentPower, player, enchantmentEntry->data, createEffectFunctor->baseObject, std::addressof(maximumPower));
+			maximumPower = std::floor(maximumPower);
+
+			if (effect == createEffectFunctor->costliestEffect)
+			{
+				power = enchantmentEntry->magnitude;
+
+				if (power > maximumPower)
+				{
+					power = maximumPower;
+				}
+				else if (power < 1.0F)
+				{
+					power = 1.0F;
+				}
+
+				if (maximumPower != enchantmentEntry->maxMagnitude)
+				{
+					power = maximumPower;
+				}
+
+				enchantmentEntry->maxMagnitude = maximumPower;
+
+				if (enchantmentEntry->filterFlag.all(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::EffectWeapon))
+				{
+					enchantmentEntry->magnitude = power;
+				}
+				else if (enchantmentEntry->filterFlag.all(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::EffectArmor))
+				{
+					power = maximumPower * createEffectFunctor->soulGemRatio;
+
+					if (power < 1.0F)
+					{
+						power = 1.0F;
+					}
+
+					enchantmentEntry->magnitude = power;
+				}
+			}
+			else
+			{
+				power = maximumPower;
+
+				if (enchantmentEntry->filterFlag.all(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::EffectArmor))
+				{
+					power = maximumPower * createEffectFunctor->soulGemRatio;
+
+					if (power < 1.0F)
+					{
+						power = 1.0F;
+					}
+				}
+			}
+		}
+
+		if (powerAffectsMagnitude)
+		{
+			magnitude = power;
+		}
+		else if (powerAffectsDuration)
+		{
+			duration = static_cast<std::uint32_t>(std::round(power));
+		}
+
+		REX::SetEffectMagnitude(&createEffectFunctor->createdEffects[createdEffectIndex], magnitude);
+		REX::SetEffectDuration(&createEffectFunctor->createdEffects[createdEffectIndex], duration);
+
+		return RE::BSContainer::ForEachResult::kContinue;
     };
 
     REL::Relocation<decltype(ImproveAllEnchantmentEffects::AddEffect)> ImproveAllEnchantmentEffects::originalAddEffect_{};
